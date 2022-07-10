8630dd4813443d4f04296ea76173ad4a87154bbd
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-07-10 03:08:29 -0300

    Agregar clase "Lista"

--- Listas/Cursores/Python.py
+++ Listas/Cursores/Python.py
@@ -8,3 +8,19 @@ class Nodo:
     self.dato = None
     self.anterior = None
     self.proximo = None
+
+class Lista:
+  def __init__(self, TipoClave, alSize): # Lista.Crear() 
+    '''Crea una lista vacía''' 
+    if alSize >= MIN and alSize <= MAX:
+      self._cursor = [''] * (alSize + 1) # La posicion 0 no la usamos pero hay que tenerla en cuenta | Reemplaza SetLength
+      for Q in range(MIN, alSize - 1):   # Encadenamientos de Libres
+        self._cursor[Q].proximo = Q + 1
+      self._cursor[alSize].proximo = NULO
+      
+      self._inicio = NULO
+      self._final = NULO
+      self._libre = MIN
+      self._Q_Items = 0
+      self._TDatoDeLaClave = TipoClave
+      self._Size = alSize
