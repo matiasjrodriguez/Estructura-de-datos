0c94064bf489ab5d06d37373a6ccad4808be9a8a
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-07-10 03:53:08 -0300

    Arreglar error en inicialización de lista

--- Listas/Cursores/Python.py
+++ Listas/Cursores/Python.py
@@ -1,4 +1,4 @@
-from Tipos.python_tipos import TipoDatosClave
+from Tipos.python_tipos import TipoDatosClave, TipoElemento, Resultado
 
 MIN = 1
 MAX = 2000
@@ -15,9 +15,13 @@ class Lista:
   def __init__(self, TipoClave:TipoDatosClave, alSize:int): # Lista.Crear() 
     '''Crea una lista vacía''' 
     if alSize >= MIN and alSize <= MAX:
-      self._cursor = [''] * (alSize + 1) # La posicion 0 no la usamos pero hay que tenerla en cuenta | Reemplaza SetLength
-      for Q in range(MIN, alSize - 1):   # Encadenamientos de Libres
-        self._cursor[Q].proximo = Q + 1
+      self._cursor = []
+      for Q in range(alSize + 1): # La posicion 0 no la usamos pero hay que tenerla en cuenta | Reemplaza SetLength
+        nodo = Nodo()
+        self._cursor.append(nodo)
+        if Q > 0:
+          self._cursor[Q].proximo = Q + 1 # Encadenamientos de Libres
+        
       self._cursor[alSize].proximo = NULO
       
       self._inicio = NULO
@@ -41,5 +45,5 @@ class Lista:
       Q = NULO
     else:
       Q = self._cursor[P].proximo
-      
+
     return Q
