2ddeed3ca48b600c32f61808d33eb25a4161caf5
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-07-10 02:57:00 -0300

    Agregar enumerado "TipoDatosClave"

--- Tipos/python_tipos.py
+++ Tipos/python_tipos.py
@@ -10,3 +10,9 @@ class Resultado(Enum):
     Otro=6
     ClaveIncompatible=7
     ClaveDuplicada=8
+
+class TipoDatosClave(Enum):
+  '''Tipos de Datos Soportados x la clave'''
+  NUMERO=1
+  CADENA=2
+  OTROS=3
